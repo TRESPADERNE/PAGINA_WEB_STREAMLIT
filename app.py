eff93e33c70623ea460a98b21ace6aa8660bba86
TRESPADERNE <felix.miguel@uva.es>
2025-05-20 17:57:23 +0200

    tabs

--- app.py
+++ app.py
@@ -11,30 +11,43 @@ if st.query_params.get("reset") == "true":
     # clear_leeCelda()
     st.query_params.reset = "false"
 
-st.session_state.contador += 1
 spreadsheet = autentica()
 
-worksheet = spreadsheet.worksheet("Grupo B") 
-partidos = leerResultados(worksheet)
-
-# st.write(partidos)
 st.markdown(estilosCSS, unsafe_allow_html=True)
 
+tab1, tab2, tab3, tab4 = st.tabs(["Grupo A", "Grupo B", "Fase Oro", "Fase Plata"])
+
+with tab1:
+    worksheet = spreadsheet.worksheet("Grupo A") 
+    partidos = leerResultados(worksheet)
 
 
-# --- Título de la App ---
-st.subheader("Resultados de Partidos")
-
-# --- Mostrar cada partido ---
-if not partidos:
-    st.warning("No hay datos de partidos para mostrar.")
-else:
-    # Generar todo el HTML junto para evitar múltiples llamadas a st.markdown si es posible
-    # html_completo = "".join([crear_html_partido(p) for p in partidos])
-    # st.markdown(html_completo, unsafe_allow_html=True)
-    # Nota: Streamlit puede manejar mejor llamadas separadas para re-renderizados parciales
-    for partido_data in partidos:
-        html_partido = crear_html_partido(partido_data)
-        st.markdown(html_partido, unsafe_allow_html=True)
+    # --- Mostrar cada partido ---
+    if not partidos:
+        st.warning("No hay datos de partidos para mostrar.")
+    else:
+        # Generar todo el HTML junto para evitar múltiples llamadas a st.markdown si es posible
+        # html_completo = "".join([crear_html_partido(p) for p in partidos])
+        # st.markdown(html_completo, unsafe_allow_html=True)
+        # Nota: Streamlit puede manejar mejor llamadas separadas para re-renderizados parciales
+        for partido_data in partidos:
+            html_partido = crear_html_partido(partido_data)
+            st.markdown(html_partido, unsafe_allow_html=True)
 
 
+with tab2:
+    worksheet = spreadsheet.worksheet("Grupo B") 
+    partidos = leerResultados(worksheet)
+
+
+    # --- Mostrar cada partido ---
+    if not partidos:
+        st.warning("No hay datos de partidos para mostrar.")
+    else:
+        # Generar todo el HTML junto para evitar múltiples llamadas a st.markdown si es posible
+        # html_completo = "".join([crear_html_partido(p) for p in partidos])
+        # st.markdown(html_completo, unsafe_allow_html=True)
+        # Nota: Streamlit puede manejar mejor llamadas separadas para re-renderizados parciales
+        for partido_data in partidos:
+            html_partido = crear_html_partido(partido_data)
+            st.markdown(html_partido, unsafe_allow_html=True)
