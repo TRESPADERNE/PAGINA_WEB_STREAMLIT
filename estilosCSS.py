83ab17163958ad1804666c6123c54dda8e3e3bcd
TRESPADERNE <felix.miguel@uva.es>
2025-05-28 16:17:55 +0200

    kk

--- estilosCSS.py
+++ estilosCSS.py
@@ -369,7 +369,7 @@ def estilosCSSCabecera():
         display: flex;
         flex-direction: column;
         align-items: center;
-        margin-bottom: 2px;
+        margin-bottom: 4px;
         margin-top: 0px;
     }
 
